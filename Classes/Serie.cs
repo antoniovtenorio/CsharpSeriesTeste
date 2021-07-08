179d19436704a36a71b6fa90134544e0bef21a20
Antonio Tenorio <antonio.v.tenorio@outlook.com>
2021-07-07 22:37:59 -0300

    Created Repository and Interface as well as updated series and main program

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -16,6 +16,11 @@ namespace DIO.Series
 
         private int Ano { get; set; }
 
+        private bool Excluido { get; set; }
+        
+        
+
+
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
             this.Id = id;
@@ -23,6 +28,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -32,6 +38,7 @@ namespace DIO.Series
             retorno += "Título: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
             retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluído: " + this.Excluido + Environment.NewLine;
             return retorno; 
         }
 
@@ -43,6 +50,13 @@ namespace DIO.Series
         public int retornaId()
         {
             return this.Id;
+        }
+        public bool retornaExcluido() {
+            return this.Excluido;
+        }
+
+        public void Excluir() {
+            this.Excluido = true;
         }
     }
 
